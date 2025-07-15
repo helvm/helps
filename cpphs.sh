302c62b6d524026b65fd03e9661b42e106d0ccc8
Kamil Adam <kamil.adam.zabinski@gmail.com>
2025-07-15 20:17:10 +0200

    Add type compilers

--- cpphs.sh
+++ cpphs.sh
@@ -5,5 +5,11 @@ cpphs --noline --strip -DEMBEDDED   examples/mini-haskell/original/classy.hs > h
 cpphs --noline --strip -DSTANDALONE examples/mini-haskell/original/classy.hs > examples/mini-haskell/standalone/classy.hs
 
 mkdir -p examples/compiler/standalone/
+cpphs --noline --strip -DEMBEDDED   examples/compiler/original/typically.hs > hs/src/HelVM/HelPS/Compiler/Typically.hs
+cpphs --noline --strip -DSTANDALONE examples/compiler/original/typically.hs > examples/compiler/standalone/typically.hs
+
 cpphs --noline --strip -DEMBEDDED   examples/compiler/original/classy.hs > hs/src/HelVM/HelPS/Compiler/Classy.hs
 cpphs --noline --strip -DSTANDALONE examples/compiler/original/classy.hs > examples/compiler/standalone/classy.hs
+
+cpphs --noline --strip -DEMBEDDED   examples/compiler/original/barely.hs > hs/src/HelVM/HelPS/Compiler/Barely.hs
+cpphs --noline --strip -DSTANDALONE examples/compiler/original/barely.hs > examples/compiler/standalone/barely.hs
