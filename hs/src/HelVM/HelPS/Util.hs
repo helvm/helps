69cdb79ab4d30daab838d8eac82e1c1fb64f7573
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-02-09 22:46:53 +0100

    Remove relude

--- hs/src/HelVM/HelPS/Util.hs
+++
@@ -1,9 +0,0 @@
-module HelVM.HelPS.Util where
-
-import           Control.Applicative.Tools
-
-mapTextWithString :: (String -> String) -> Text -> Text
-mapTextWithString f = toText . f . toString
-
-mapTextWithStringF :: Functor f => (String -> f String) -> Text -> f Text
-mapTextWithStringF f = toText <.> f . toString
